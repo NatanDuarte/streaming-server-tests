d6052068f8bafd524fed4fba5f79c98c888d26be
CarlaKremer <carlakremerf@gmail.com>
2023-03-23 23:28:24 -0300

    react video test

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactPlayer from 'react-player'
+
+function App() {
+  return (
+    <div className="App">
+        <ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U' />
+    </div>
+  );
+}
+
+export default App;
